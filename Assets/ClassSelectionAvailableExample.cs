a27598f2cb2ad3f57837a750ece35ff96ed1602d
Mara <mara@github.com>
2025-05-04 15:05:29 +0300

    Final push

--- Assets/ClassSelectionAvailableExample.cs
+++ Assets/ClassSelectionAvailableExample.cs
@@ -7,54 +7,47 @@ public class ClassSelectionAvailableExample : MonoBehaviour
 {
     private uint _selectedClass = 0;
     private bool _update = false;
+
+    public AudioSource[] classAudioSources; // Array for 4 audio sources
+
     void Start()
     {
-        //attach to class selection available event
         BCIManager.Instance.ClassSelectionAvailable += OnClassSelectionAvailable;
+
+        // Optional: Validate array size
+        if (classAudioSources.Length < 4)
+        {
+            Debug.LogWarning("Not enough audio sources assigned to handle all classes.");
+        }
     }
 
     void OnApplicationQuit()
     {
-        //detach from class selection available event
         BCIManager.Instance.ClassSelectionAvailable -= OnClassSelectionAvailable;
     }
 
     void Update()
     {
-        //TODO ADD YOUR CODE HERE
-        if(_update)
+        if (_update)
         {
-            switch (_selectedClass)
+            if (_selectedClass < classAudioSources.Length && classAudioSources[(int)_selectedClass] != null)
             {
-                case 0:
-                    break;
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-
+                classAudioSources[(int)_selectedClass].Play();
+                Debug.Log("Playing audio for class " + _selectedClass);
             }
+            else
+            {
+                Debug.LogWarning("No audio assigned for class " + _selectedClass);
+            }
+
             _update = false;
-        } 
+        }
     }
 
-    /// <summary>
-    /// This event is called whenever a new class selection is available. Th
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
     private void OnClassSelectionAvailable(object sender, EventArgs e)
     {
         ClassSelectionAvailableEventArgs ea = (ClassSelectionAvailableEventArgs)e;
-       _selectedClass = ea.Class;
+        _selectedClass = ea.Class;
         _update = true;
         Debug.Log(string.Format("Selected class: {0}", ea.Class));
     }
